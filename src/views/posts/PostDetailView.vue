b12e582f1e80c4b63f8002568ca28da949c48d50
지영서 <estell.ys.j@gmail.com>
2025-06-11 10:50:05 +0900

    이름을 가지는 뷰 라우터 기본 성정하기
    페이지 이동 기본 셋팅

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,10 +1,56 @@
 <template>
 	<div>
-		<h2>게시글 상세</h2>
+		<h2>제목</h2>
+		<p>내용</p>
+		<p class="text-muted">2020-01-01</p>
 		<hr class="my-4" />
+		<div class="row g-2">
+			<div class="col-auto">
+				<button class="btn btn-outline-dark" @click="prePost">이전글</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-dark" @click="nextPost">다음글</button>
+			</div>
+			<div class="col-auto me-auto"></div>
+			<div class="col-auto">
+				<button class="btn btn-outline-dark" @click="goPostListPage">
+					목록
+				</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-primary" @click="postEditPage">
+					수정
+				</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-danger" @click="postDelete">삭제</button>
+			</div>
+		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+const id = route.params.id;
+
+// 목록이동
+const goPostListPage = () => {
+	router.push({
+		name: "PostList",
+	});
+};
+
+// 게시글 수정
+const postEditPage = () => {
+	router.push({
+		name: "PostEdit",
+		params: { id: id },
+	});
+};
+</script>
 
 <style lang="scss" scoped></style>
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -2,9 +2,42 @@
 	<div>
 		<h2>게시글 수정</h2>
 		<hr class="my-4" />
+		<form action="">
+			<div class="mb-3">
+				<label for="title" class="form-label">제목</label>
+				<input type="text" class="form-control" id="title" />
+			</div>
+			<div class="mb-3">
+				<label for="content" class="form-label">내용</label>
+				<textarea class="form-control" id="content" rows="3"></textarea>
+			</div>
+			<div class="pt-4">
+				<button
+					type="button"
+					class="btn btn-outline-danger me-2"
+					@click="goPostDetailPage"
+				>
+					취소
+				</button>
+				<button class="btn btn-primary">수정</button>
+			</div>
+		</form>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+const id = route.params.id;
+
+const goPostDetailPage = () => {
+	router.push({
+		name: "PostDetail",
+		params: { id: id },
+	});
+};
+</script>
 
 <style lang="scss" scoped></style>
